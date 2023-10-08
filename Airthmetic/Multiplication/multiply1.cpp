7c7df36fc8c478b8672f97387028640d2b0cb8fe
PRAKASH <144153979+itsThePrakash@users.noreply.github.com>
2023-10-08 10:53:20 +0530

    Add files via upload

---
+++ Airthmetic/Multiplication/multiply1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+
+int main (){
+    while(true){
+         
+         int first, second, product;
+         cout << "enter two number ";
+         cin >> first >> second;
+         product = first*second;
+         cout<< first<< "*"<<second<<"="<<product;
+    }
+}
